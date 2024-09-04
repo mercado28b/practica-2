e0d487d5080a176702f1287f06787d27392c1ea8
mercado28b <alu.22130565@correo.itlalaguna.edu.mx>
2024-09-04 17:48:21 -0600

    practica 2

---
+++ src/practica2_java.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class practica2_java {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton sumarButton;
+    private JButton restarButton;
+    private JButton multiplicarButton;
+    private JButton dividirButton;
+    private JButton borrarButton;
+
+    public practica2_java() {
+        sumarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        restarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        multiplicarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        dividirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
